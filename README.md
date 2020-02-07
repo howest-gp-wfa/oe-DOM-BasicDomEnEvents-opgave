77f2da06549345614b91a1342a1142f92773e71e
Mileto <mileto.di.marco@howest.be>
2020-02-07 13:03:20 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# DOM-elementen aanmaken in runtime
+Opgave om label-input paren aan te maken in runtime.
+De opgave staat in commentaar in de opgave. Wees kritisch en plaats de code eventueel op een andere plaats dan de opgave in het JS-bestand (vb. declaratie van variabelen, toekenning van waarden ...).
+Zie ook de screenshots in de map img om een beeld te hebben van de werking van de code
+
---
+++ js/script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+var txtOmschrijving;
+var txtVeldNaam;
+
+window.addEventListener('load', Initieer);
+
+function Initieer() {
+  //Declareer en initialiseer een variabele om de knop in bij te houden
+
+  // Sla de DOM-elementen met id txtOmschrijving en txtVeldNaam op in hun resp. variabelen
+
+  // Vul in txtOmschrijving en txtVeldNaam standaardwaarden in, nl. Adres en txtAdres
+
+  // Voeg een eventlisteners toe aan de knop om VoegVeldToe te laten uitvoeren bij een klik
+}
+
+function VoegVeldToe() {
+  /*
+  Declareer en initialiseer een variabele om divNieuweElementen in bij te houden
+
+  Declareer variabelen om de input in de textboxen bij te houden en sla de input erin op
+
+  Declareer een variabele met de naam newDiv om een nieuw div-element in op te slaan
+  De css-classes van deze variabele zijn input en accent
+
+  Declareer een variabele met de naam newLabel om een nieuw label-element in op te slaan
+  In dit label verschijnt de omschrijving die opgegeven is door de gebruiker
+  Het for-attribuut wordt ingevuld
+  Het label wordt toegevoegd aan newDiv
+
+  Declareer een variabele met de naam newInput om een nieuw input-element in op te slaan
+  De id- en type-attributen worden ingevuld
+  De input wordt toegevoegd aan newDiv
+
+  De input in de textboxes wordt gewist
+
+  newDiv wordt toegevoegd aan divNieuweElementen
+
+  */
+}
+
+
